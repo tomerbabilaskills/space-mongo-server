a9c3ac78dabea930be15d4324e17f8b7de805f68
Tomer Babila <tomerb@academy-skills.com>
2021-07-25 17:29:00 +0300

    Find system name and all visitors of planet

--- api/planetRouter.js
+++ api/planetRouter.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const mongoose = require('mongoose');
 const planetModel = require('../models/planetModel');
 const visitorModel = require('../models/visitorModel');
+const solarSystemModel = require('../models/solarSystemModel');
 
 const router = Router();
 
@@ -22,6 +23,50 @@ router.get('/:id?', async (req, res) => {
         },
       },
       { $project: { _id: 1, name: 1, visitors: { _id: 1, name: 1 } } },
+    ];
+
+    if (planetId) pipeline.unshift({ $match: { _id: ObjectId(planetId) } });
+
+    let response = await planetModel.aggregate(pipeline);
+    response = response.length === 1 ? response[0] : response;
+
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// [GET] - find system name and all visitors of planet
+router.get('/system-visitors/:id?', async (req, res) => {
+  const planetId = req.params.id;
+
+  try {
+    const pipeline = [
+      {
+        $lookup: {
+          from: visitorModel.collection.name,
+          localField: 'visitors',
+          foreignField: '_id',
+          as: 'visitors',
+        },
+      },
+      {
+        $lookup: {
+          from: solarSystemModel.collection.name,
+          localField: 'system',
+          foreignField: '_id',
+          as: 'system',
+        },
+      },
+      { $unwind: '$system' },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          visitors: { _id: 1, name: 1 },
+          system: { _id: 1, starName: 1 },
+        },
+      },
     ];
 
     if (planetId) pipeline.unshift({ $match: { _id: ObjectId(planetId) } });
